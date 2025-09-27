da8f0634e3bb88b7f58e9f6457ec8679ed0ff5a0
Abdullah Nasr <abdullah.nasr1022@gmail.com>
2025-09-27 19:40:06 +0300

    abd: add cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const cors = require("cors")
 const PORT = process.env.PORT;
 
 // ================Import-Routes====================
@@ -16,7 +17,7 @@ connectDB();
 
 //==============Middleware===============
 app.use(express.json());
-
+app.use(cors())
 
 // =============Routes====================
 app.use("/products", productRoutes);
